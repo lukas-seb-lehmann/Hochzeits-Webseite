b61db2b22f967814120786f700a9541068924e11
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2021-10-21 18:00:51 +0200

    cookie push

--- js/function.js
+++ js/function.js
@@ -29,4 +29,56 @@ function closemenu(){
 
 
 
+window.onload=checkcookie();
+
+
+function starttracking(){
 	
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'G-SRC8XTZ052', 'auto');
+ga('send', 'pageview');
+	
+}
+
+ 
+
+
+ function checkcookie() {
+	 if (document.cookie.indexOf("nocookie") <= 0) {
+    //new visit
+	
+	
+  }else{
+	  
+	//returning
+	var containerelement = document.getElementById("container");
+	containerelement.classList.remove("active");
+	starttracking();
+  }
+ }
+
+function funzustimmen(){
+	document.cookie = "name=nocookie";
+	var containerelement = document.getElementById("container");
+	containerelement.classList.remove("active");
+	
+	starttracking();
+
+	
+	
+}
+
+function funanpassen(){
+	var containerelement = document.getElementById("container");
+	containerelement.classList.remove("active");
+	
+}
+
+function deletecookie(){
+	document.cookie = "name=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+}
+ 
