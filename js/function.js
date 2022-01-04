f454a9aa049ae86ddf485e1a2ee9f2fd3ca320b1
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2022-01-04 17:24:40 +0100

    pixel standard

--- js/function.js
+++ js/function.js
@@ -48,17 +48,7 @@ ga('send', 'pageview');*/
 	
 }
 
-function startpixel(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '619399562708209');
-fbq('track', 'PageView');
-
+f
 
 
 
@@ -73,7 +63,7 @@ fbq('track', 'PageView');
 	var containerelement = document.getElementById("container");
 	containerelement.classList.remove("active");
 	starttracking();
-	startpixel();
+	
   }
  }
 
@@ -88,8 +78,7 @@ function funzustimmen(){
 	containerelement.classList.remove("active");
 	
 	starttracking();
-startpixel();
-	
+
 	
 }
 
