5eebbcc7a6b363b1927c55225abfd860b803dcff
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2022-01-04 17:06:00 +0100

    id update

--- js/function.js
+++ js/function.js
@@ -56,7 +56,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '985667478700652');
+fbq('init', '619399562708209');
 fbq('track', 'PageView');
 
 
