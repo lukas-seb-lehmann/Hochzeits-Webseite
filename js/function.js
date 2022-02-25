46a1fb6c65499980bc362a8c8b6ea5309f86d21d
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2022-02-25 22:34:54 +0100

    Update function.js

--- js/function.js
+++ js/function.js
@@ -51,7 +51,7 @@ function starttracking(){
 	
 	document.getElementById("facebook-code").append('!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version="2.0"; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,"script", "https://connect.facebook.net/en_US/fbevents.js"); fbq("consent", "grant"); fbq("init", "589544735010993"); fbq("track", "PageView");');
 
-	
+	location.reload();
 	
 }
 
