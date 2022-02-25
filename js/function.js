d519c00a040c00eb2ac3f0ca3896a4123c620856
Lukas Lehmann <56516534+lukas-seb-lehmann@users.noreply.github.com>
2022-02-25 21:47:24 +0100

    facbook code test 2

--- js/function.js
+++ js/function.js
@@ -56,22 +56,7 @@ ga('send', 'pageview');*/
 	
 	
 	
-	/*document.getElementsByTagName('head').append('<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('consent', 'revoke');
-fbq('init', '589544735010993');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=589544735010993&ev=PageView&noscript=1"
-/></noscript>')*/
+document.getElementsByid('facebook-code').innerHTML ='!function(f,b,e,v,n,t,s) {if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('consent', 'revoke'); fbq('init', '589544735010993'); fbq('track', 'PageView');'
 	
 	
 }
